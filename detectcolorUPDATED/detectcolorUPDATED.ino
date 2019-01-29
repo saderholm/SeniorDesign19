55b374eaad0f4b7f95b9bd4bf03d4c895dd58737
Unknown <saderholms@gmail.com>
2019-01-29 17:30:17 -0500

    updated color detection code
    changed range, deleted extraneous lines

--- detectcolorUPDATED/detectcolorUPDATED.ino
+++ detectcolorUPDATED/detectcolorUPDATED.ino
@@ -2,7 +2,7 @@
 // Date: January 29, 2019
 // This program identifies color of object from TCS3200 sensor input
 // Optimal range: 
-/ /Code modified from: https://www.instructables.com/id/Arduino-Color-Detection/
+// Code modified from: https://www.instructables.com/id/Arduino-Color-Detection/
 
 // Define TCS3200 pin wired to Arduino
 #define S0 2
@@ -27,12 +27,12 @@ void setup() {
   pinMode(sensorOut, INPUT);
   
   // Setting frequency scaling to 20%
-  digitalWrite(S0,HIGH);
-  digitalWrite(S1,LOW); 
+   digitalWrite(S0,HIGH);
+   digitalWrite(S1,LOW); 
 
-  // Setting frequency scaling to 100%                 // TRY THIS
-  //digitalWrite(S0,HIGH);
-  //digitalWrite(S1,HIGH); 
+  // Setting frequency scaling to 100%                 // Will not work because frequency values were found with 20% scaling
+//  digitalWrite(S0,HIGH);
+//  digitalWrite(S1,HIGH); 
   
   // Begins serial communication
   Serial.begin(9600);
@@ -52,9 +52,7 @@ void loop() {
     blu = pulseIn(sensorOut, LOW); // Reading BLUE component of color
 
 // RGB frequency range for white
-  if (red > 11  && red < 14   &&  grn >  12 && grn < 15    &&  blu > 10  && blu < 13)   Serial.println("WHITE");
-
-  //else if (red > 80 && red < 125  &&  grn > 90 && grn < 125   &&  blu > 80 && blu < 125)  Serial.println("BLACK");
+  if (red < 25  && grn < 25  && blu < 25)   Serial.println("WHITE");
 
   // RGB frequency range for red
   else if (red > 25 && red < 95   &&  grn > 106 && grn < 165    &&  blu > 71 && blu < 150)   Serial.println("RED");
@@ -68,6 +66,6 @@ void loop() {
   // RGB frequency range for BLUE
   else if (red > 62 && red < 89  &&  grn > 28 && grn < 140   &&  blu > 21 && blu < 134)   Serial.println("BLUE");
 
-  //else  Serial.println("NO_COLOR");
+  else  Serial.println("CARPET");
 
 }
